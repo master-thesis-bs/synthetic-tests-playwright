3aa7d06b6b219dfc274d46f2c981c263f0f7af69
starzyk.bartlomiej <starzyk.bartlomiej@gmail.com>
2024-04-29 23:32:34 +0200

    playwright config cleanup

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  await page.goto('http://34.118.94.255/');
 
   await expect(page).toHaveTitle(/timeline-app/);
 });
