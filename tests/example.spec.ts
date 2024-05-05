eb6c844a6e7986b14cd6d447f6e32c4bc07b59b4
starzyk.bartlomiej <starzyk.bartlomiej@gmail.com>
2024-05-05 20:26:29 +0200

    check error message alert

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -3,5 +3,5 @@ import { test, expect } from '@playwright/test';
 test('has title', async ({ page }) => {
   await page.goto('https://www.google.com/');
 
-  await expect(page).toHaveTitle(/Google/);
+  await expect(page).toHaveTitle(/Google123/);
 });
